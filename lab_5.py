73f3337e1f5de32275d151bb08e7bf3f1a22d547
MatthewMango <98561675+MatthewMango@users.noreply.github.com>
2022-04-27 07:17:31 -0700

    finished psuedo-code

--- lab_5.py
+++ lab_5.py
@@ -17,4 +17,24 @@ After each word is translated, we then
 print out the translated sentence to the user.
 """
 
+"""
+main():
+    set dictionary = translate()
+    translate()
 
+translate(sentence, dictionary):
+
+    words = for each of the word in the sentence
+
+    for each words, translate the word
+    print translated sentence to user
+
+create_dictionary():
+    read in textese.txt
+    create list = each line from file
+    close the file
+    create a dict off of the list
+    return the dict
+
+main()
+"""
